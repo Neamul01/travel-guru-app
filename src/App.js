bb09c7788031abfd1fc1df54b0d81194dcb284ee
Neamul01 <neamulhaque2002@gmail.com>
2022-04-16 01:25:29 +0600

    more component created and header designed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Booking from './pages/Booking/Booking';
 import Search from './pages/Search/Search';
 import Login from './pages/Login/Login';
-import NotFound from './pages/Shared/NotFound'
+import NotFound from './pages/Shared/NotFound/NotFound'
+import Header from './pages/Shared/Header/Header';
+import Blog from './pages/Blog/Blog';
+import Destination from './pages/Destination/Destination';
+import News from './pages/News/News';
+import Contact from './pages/Contact/Contact';
 
 function App() {
   return (
     <div >
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/booking' element={<Booking></Booking>}></Route>
         <Route path='/search' element={<Search></Search>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/destination' element={<Destination></Destination>}></Route>
+        <Route path='/news' element={<News></News>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
+        <nav className='flex border my-10 p-6'>
+            <input className='py-2 px-4' type="text" placeholder='Search Here' />
+            <Link className='py-2 px-4' to={'/news'}>News</Link>
+            <Link className='py-2 px-4' to={'/destination'}>Destination</Link>
+            <Link className='py-2 px-4' to={'/blog'}>Blog</Link>
+            <Link className='py-2 px-4' to={'/contact'}>Contact</Link>
+            <Link className='py-2 px-4' to={'login'}>Login</Link>
+        </nav>
     );
 };
 
