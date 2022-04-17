5a793fc50c1ac874053051740f211f5a8c67f5a4
Neamul01 <neamulhaque2002@gmail.com>
2022-04-17 14:23:29 +0600

    header change

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../../../logo.png'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
+    const location = useLocation()
+    const currentLocation = location.pathname;
+    console.log(currentLocation)
     return (
-        <nav className='flex items-center justify-center border-none pt-8 relative text-white'>
+        <nav className='flex items-center justify-between px-40 border-none pt-8 relative  text-white'
+            style={{
+                backgroundColor: (currentLocation === '/signup' || currentLocation === '/login') && 'white',
+                color: (currentLocation === '/login' || currentLocation === '/signup') && 'black'
+            }}>
             <Link to={'/'}>
                 <img className='w-32 h-14 mr-20' src={logo} alt="logo" />
             </Link>
-            <div className='relative'>
-                <input
-                    className='py-2 px-5 pl-9 mx-2 border-white border-2 outline-none h-11 w-96 rounded-md search-custom'
-                    style={{ background: 'rgba(255, 255, 255, 0.249)' }} type="text" placeholder='Search Your Destination...' />
-                <SearchIcon className='absolute left-5 top-3 ' />
+            <div className='flex'>
+                <div className='relative' style={{ display: (currentLocation === '/login' || currentLocation === '/signup') && 'none' }}>
+                    <input
+                        className='py-2 px-5 pl-9 mx-2 border-white border-2 outline-none h-11 w-96 rounded-md search-custom'
+                        style={{
+                            background: 'rgba(255, 255, 255, 0.249)'
+                        }} type="text" placeholder='Search Your Destination...' />
+                    <SearchIcon className='absolute left-5 top-3 ' />
+                </div>
+                <Link className='py-2 px-5 mx-2' to={'/news'}>News</Link>
+                <Link className='py-2 px-5 mx-2' to={'/destination'}>Destination</Link>
+                <Link className='py-2 px-5 mx-2' to={'/blog'}>Blog</Link>
+                <Link className='py-2 px-5 mx-2' to={'/contact'}>Contact</Link>
+                <Link className='py-2 px-6 mx-2 bg-yellow-500 rounded-md' to={'login'}>Login</Link>
             </div>
-            <Link className='py-2 px-5 mx-2' to={'/news'}>News</Link>
-            <Link className='py-2 px-5 mx-2' to={'/destination'}>Destination</Link>
-            <Link className='py-2 px-5 mx-2' to={'/blog'}>Blog</Link>
-            <Link className='py-2 px-5 mx-2' to={'/contact'}>Contact</Link>
-            <Link className='py-2 px-6 mx-2 bg-yellow-500 rounded-md' to={'login'}>Login</Link>
-        </nav>
+        </nav >
     );
 };
 
